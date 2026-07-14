c9d21e3777d8fee67eefe70fbd26bc7a787c3364
Raj Shah <rajrulz@aol.com>
2026-07-14 12:29:38 -0400

    fix: keep OCR tests out of production routes

--- web/tests/ocr.test.ts
+++ web/tests/ocr.test.ts
@@ -20,7 +20,7 @@ import {
   shouldRetryOcrWithPdfVision,
   shouldUsePdfVision,
   validateOcrInput,
-} from './ocr'
+} from '../api/ocr'
 
 const JPEG = new Uint8Array([0xff, 0xd8, 0xff, 0xe0, 0, 0])
 const PNG = new Uint8Array([0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a, 0, 0])
